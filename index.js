4c40ee5f68ec7ab4fe436227396f037dd3192ed2
张宇 <wb_zhangyu28@meituan.com>
2022-07-20 16:09:45 +0800

    feature: 迁移提交

---
+++ index.js
@@ -0,0 +1,14 @@
+/*
+ * @Author: zhangyu
+ * @Date: 2021-04-17 18:36:24
+ * @LastEditTime: 2021-04-28 16:11:02
+ */
+
+module.exports = {
+    // 启动文件
+    start: require('./lib/start.js'),
+    // 控制器
+    Controller: require('./lib/controller.js'),
+    // 模型
+    Model: require('./lib/model.js')
+}
